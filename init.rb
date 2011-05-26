da4272fccab29aea3ee598a4544ab362a1bc74f7
s-andy <s-andy@2807ba61-5de5-4062-896f-e419b1c8a63b>
2011-05-26 22:07:33 +0000

    Moved RHTML into separate files, fixed crash with new extended profile

--- init.rb
+++ init.rb
@@ -7,10 +7,10 @@ RAILS_DEFAULT_LOGGER.info 'Starting Author Plugin for Redmine'
 Redmine::Plugin.register :author_plugin do
     name 'Author box'
     author 'Andriy Lesyuk'
-    author_url 'http://www.facebook.com/andriy.lesyuk'
+    author_url 'http://www.andriylesyuk.com/'
     description 'Adds box with the author photo, name etc into the sidebar.'
-    url 'http://labs.softjourn.com/projects/redmine-author'
-    version '0.0.1'
+    url 'http://projects.andriylesyuk.com/projects/redmine-author'
+    version '0.0.2'
 
-    settings :default => {:author_role_id => nil}, :partial => 'settings/author_role'
+    settings :default => { :author_role_id => nil }, :partial => 'settings/author_role'
 end
