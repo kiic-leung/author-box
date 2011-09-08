2cb10bb2c59361863ace52ec6a4f999f1ff41f47
s-andy <s-andy@2807ba61-5de5-4062-896f-e419b1c8a63b>
2011-09-08 08:29:05 +0000

    Spanish and Chinese translations

--- init.rb
+++ init.rb
@@ -10,7 +10,7 @@ Redmine::Plugin.register :author_plugin do
     author_url 'http://www.andriylesyuk.com/'
     description 'Adds box with the author photo, name etc into the sidebar.'
     url 'http://projects.andriylesyuk.com/projects/redmine-author'
-    version '0.0.2'
+    version '0.0.3'
 
     settings :default => { :author_role_id => nil }, :partial => 'settings/author_role'
 end
