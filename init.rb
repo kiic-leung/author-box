999f05770bab51183f96e12a9c407111cbed538b
s-andy <s-andy@2807ba61-5de5-4062-896f-e419b1c8a63b>
2012-09-20 21:01:44 +0000

    First fixed for Redmine 1.4.x and later

--- init.rb
+++ init.rb
@@ -2,15 +2,15 @@ require 'redmine'
 
 require_dependency 'author_hook'
 
-RAILS_DEFAULT_LOGGER.info 'Starting Author Plugin for Redmine'
+Rails.logger.info 'Starting Author Plugin for Redmine'
 
-Redmine::Plugin.register :author_plugin do
+Redmine::Plugin.register :author do
     name 'Author box'
     author 'Andriy Lesyuk'
     author_url 'http://www.andriylesyuk.com/'
     description 'Adds box with the author photo, name etc into the sidebar.'
     url 'http://projects.andriylesyuk.com/projects/redmine-author'
-    version '0.0.3'
+    version '0.0.4'
 
     settings :default => { :author_role_id => nil }, :partial => 'settings/author_role'
 end
