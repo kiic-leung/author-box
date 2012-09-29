2dcd09764d778bb6a7bfa4c337550362badf3099
s-andy <s-andy@2807ba61-5de5-4062-896f-e419b1c8a63b>
2012-09-29 10:10:54 +0000

    Changed version to version:0.1.0

--- init.rb
+++ init.rb
@@ -10,7 +10,7 @@ Redmine::Plugin.register :author do
     author_url 'http://www.andriylesyuk.com/'
     description 'Adds box with the author photo, name etc into the sidebar.'
     url 'http://projects.andriylesyuk.com/projects/redmine-author'
-    version '0.0.4'
+    version '0.1.0'
 
     settings :default => { :author_role_id => nil }, :partial => 'settings/author_role'
 end
